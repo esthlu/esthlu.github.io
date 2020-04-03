4ca632bd921555a212170c8aa463ab9bc2daa5c8
Esther Lu <eeyan14@gmail.com>
2020-04-02 21:44:57 -0400

    new template pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './views/Home';
+import About from './views/About';
+import Portfolio from './views/Portfolio';
+import Experience from './views/Experience';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+
+        <Route path="/experience">
+          <Experience />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/views/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import layout from '../styles/layout.css';
+
+function About() {
+  return (
+    <div id="about">
+      <h1>about</h1>
+      <p>a work in progress</p>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default About;
