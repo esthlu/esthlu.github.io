ba418d8d8a7bf975c623d6bc94f4323d00a2f938
Esther Lu <eeyan14@gmail.com>
2020-11-21 16:26:04 -0500

    Updates

---
+++ precache-manifest.b584fa1dfd643ea81f5a381d66281549.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a655a65af4c8045d34cbcc406f7cef15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f0ba06dc27d6de50bb5",
+    "url": "/static/css/main.bc7dac66.chunk.css"
+  },
+  {
+    "revision": "8908a373bc5ec66337d2",
+    "url": "/static/js/2.a0b01d24.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a0b01d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0ba06dc27d6de50bb5",
+    "url": "/static/js/main.58aba279.chunk.js"
+  },
+  {
+    "revision": "300e41ec4c328cdf178c",
+    "url": "/static/js/runtime-main.e7676bd0.js"
+  },
+  {
+    "revision": "3d6b3435961cd70744e246ad98755fe9",
+    "url": "/static/media/logo.3d6b3435.png"
+  },
+  {
+    "revision": "f7b294c9e69ed5fbc3fa25f3b48ebdd1",
+    "url": "/static/media/profile_img.f7b294c9.jpg"
+  },
+  {
+    "revision": "a55434173ac9c7894ac5fcec0bc2076e",
+    "url": "/static/media/temp.a5543417.png"
+  }
+]);
